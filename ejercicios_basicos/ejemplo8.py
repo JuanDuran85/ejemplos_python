76b57302d5da9f4374ee90b430468f78014d1a43
JuanDuran85 <duran85juan@gmail.com>
2021-08-03 16:19:45 -0400

    trabajando con tuplas, listas y diccionarios

--- ejercicios_basicos/ejemplo8.py
+++ ejercicios_basicos/ejemplo8.py
@@ -8,3 +8,33 @@ numero2 = 34;
 print(f"La suma de dos variables es: {numero + numero2}");
 
 # Listas
+
+lista = ["Python", "Angular", "Vue", "Nest", "Node"];
+
+lista.append("Django");
+lista.insert(4, "Express");
+
+print(lista);
+
+# tuplas: son inmutables. Las tuplas cuando tienen un dato deben terminar con la coma y el espacio
+tupla1 = ("Python", "Angular", "Vue", "Nest", "Node");
+print(tupla1[1]);
+
+# diccionarios
+datos = {
+    "name": "Rocio",
+    "lastname": "Sanchez",
+    "age": "22",
+    "country": "Spain",
+    "language": "Python"
+};
+
+# agregar un dato
+datos["city"] = "Barcelona";
+# borrar un dato
+del datos["country"];
+# cantidad de claves
+print(len(datos));
+
+print(datos);
+print(datos["name"]);
