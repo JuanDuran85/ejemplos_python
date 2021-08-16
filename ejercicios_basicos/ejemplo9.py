ea54535fd39030cea953b234d7356445b9f390cd
JuanDuran85 <duran85juan@gmail.com>
2021-08-15 20:52:54 -0400

    nuevos archivos

---
+++ ejercicios_basicos/ejemplo9.py
@@ -0,0 +1,21 @@
+x = 2;
+y = 3;
+print(x + y);
+print(x - y);
+print(x * y);
+print(x / y);
+print(x % y);
+print(x ** y);
+
+print("Direccion de memoria de x");
+print(id(x));
+print("Direccion de memoria de y");
+print(id(y));
+
+print("Tipo de datos con type")
+print("El ':str' se utiliza para dar un pista del posible valor que deberia contener una varibale")
+cadena: str = "Cadena de string"
+boleano = False
+print(type(cadena))
+print(type (x))
+print(type(boleano))
