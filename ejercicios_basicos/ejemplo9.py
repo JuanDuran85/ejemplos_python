9343ce88e99736b9934af456aa68c2749b410a7f
JuanDuran85 <duran85juan@gmail.com>
2021-08-20 12:22:53 -0400

    nuevos ejemplos

--- ejercicios_basicos/ejemplo9.py
+++ ejercicios_basicos/ejemplo9.py
@@ -27,4 +27,19 @@ ancho = int(input("Ingrese el ancho: "))
 area = alto * ancho
 print("El area del rectangulo es: ", area)
 perimetro = 2 * (alto + ancho)
-print("El perimetro del rectangulo es: ", perimetro)
+print('El perimetro del \'rectangulo\' es: ', perimetro)
+
+
+# Determinar cual es el mayor de dos numeros ingresados por el usuario
+
+numero_uno = int(input("Ingrese un numero A: "))
+numero_dos = int(input("Ingrese un numero B: "))
+
+if numero_uno > numero_dos:
+    print(f"{numero_uno} es mayor que {numero_dos}. Por lo tanto, el numero mayor es: {numero_uno}")
+elif numero_dos > numero_uno:
+    print(f"{numero_dos} es mayor que {numero_uno}. Por lo tanto, el numero mayor es: {numero_dos}")
+else:
+    print("Los numeros ingresados son iguales")
+
+    
