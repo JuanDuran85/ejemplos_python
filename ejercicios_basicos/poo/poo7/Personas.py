8e680bc5ed3510d30963b2734df5aace863315b8
JuanDuran85 <duran85juan@gmail.com>
2021-09-19 21:41:52 -0300

    trabanado con modulos en POO

--- ejercicios_basicos/poo/poo7/Personas.py
+++ ejercicios_basicos/poo/poo7/Personas.py
@@ -6,7 +6,7 @@ class Persona:
         # si se utilizan los dos __ no se puede acceder a el
         self.__nombre = nombre
         self.__apellido = apellido
-        self.edad = edad
+        self.__edad = edad
     
     # para encapsular una propiedad se utiliza el decorador @property en un metodo del tipo get
     @property
@@ -21,15 +21,25 @@ class Persona:
     def apellido(self):
         return self.__apellido
     
+    @property
+    def edad(self):
+        return self.__edad
+
+    @edad.setter
+    def edad(self, edad):
+        if edad < 0:
+            raise ValueError("La edad no puede ser negativa")
+        self.__edad = edad
+
     # como no existe un metodo setter para el atributo apellido, seria entonces un atributo de solo lectura
 
     def mostrar_detalles(self):
-        print(f"El nombre es: {self.nombre} y la edad: {self.edad}")
+        print(f"El nombre es: {self.nombre}, el apellido es: {self.apellido} y la edad: {self.edad}")
 
-persona1 = Persona("Juan", 23)
+persona1 = Persona("Omer", "Dach", 23)
 persona1.mostrar_detalles()
 print(persona1.nombre)
 persona1.nombre = "Pedro"
 print(persona1.nombre)
+persona1.edad = 50
 persona1.mostrar_detalles()
-
---
+++ ejercicios_basicos/poo/poo7/PruebaPersona.py
@@ -0,0 +1,10 @@
+# con asterisco * se importa todas las clases del archivo
+
+from Personas import Persona
+
+persona1 = Persona("Juan", "Perez", 23)
+persona2 = Persona("Pedro", "Gomez", 33)
+
+persona1.mostrar_detalles()
+persona2.mostrar_detalles()
+
