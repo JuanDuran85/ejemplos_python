f6799c958a18b26ac147b39c627280f8c88daf7d
JuanDuran85 <duran85juan@gmail.com>
2021-11-05 21:30:30 -0300

    trabajando con metodos para string

--- ejercicios_basicos/tipo_datos_avanzado/leer_contenido_online.py
+++ ejercicios_basicos/tipo_datos_avanzado/leer_contenido_online.py
@@ -11,6 +11,14 @@ palabras = []
 
 with urlopen(req) as contenidos:
     data = contenidos.read().decode('utf-8')
+# convertir todo a mayusculas
+print(data.upper())
+# convertir todo a minusculas
+print(data.lower())
+# Inicia con
+print(data.startswith('En GlobalMentoring.com.mx'))
+# finaliza con
+print(data.endswith('GlobalMentoring.com.mx'))
 
 with open('GlobalMentoring.txt', 'w', encoding='utf8') as archivo:
     archivo.write(data)
@@ -24,5 +32,13 @@ with open('GlobalMentoring.txt', 'r', encoding='utf8') as archivo:
 
 print(palabras)
 
+# contando cantidad de palabras
+print(f"La cantidad de palabra 'Universidad' es: {palabras.count('Universidad')}")
 
+# islower e isupper
+string_mensaje = """
+FUNDADOR DE GLOBALMENTORING.COM.MX
+"""
+print(string_mensaje.islower())
+print(string_mensaje.isupper())
 
