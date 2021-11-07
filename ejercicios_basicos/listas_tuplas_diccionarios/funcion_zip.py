a2d8619b15ad8572ee31243c8e07d17bc9dc0dfa
JuanDuran85 <duran85juan@gmail.com>
2021-11-07 11:36:08 -0300

    trabajando con la funcion zip para listaas, tuplas y diccionarios

--- ejercicios_basicos/listas_tuplas_diccionarios/funcion_zip.py
+++ ejercicios_basicos/listas_tuplas_diccionarios/funcion_zip.py
@@ -4,7 +4,44 @@
 # usando la funcion zip de python
 lista_1 = (1,2,3,4)
 letras = ['a','b','c','d','e','f','g','h','i','j','k']
-mezcla = zip(lista_1,letras)
+identificador = 234,432,234,432
+conjunto = {2,-5,7,3,88,4-2}
+# del conjunto como es un set no se tiene orden, por lo tanto toma valores aleatorios
+mezcla = zip(lista_1,letras, identificador, conjunto)
 print(list(mezcla)) # una vez se leen y muestran los elementos de un zip, se terminan, por lo que se debe a volver a realizar el proceso
-mezcla2 = zip(lista_1,letras)
+mezcla2 = zip(lista_1,letras, identificador)
 print(tuple(mezcla2))
+
+for valores in zip(lista_1,letras, identificador, conjunto):
+    print(valores)
+    
+for v1, v2, v3, v4 in zip(lista_1,letras, identificador, conjunto):
+    print(v1, v2, v3, v4)
+    
+# unzip y unpacking
+
+mezcla_dos = [(1,'a'),(2,'b'),(3,'c'),(4,'d')]
+numeros, letras = zip(*mezcla_dos)
+print(f"Numeros: {numeros}")
+print(f"Letras: {letras}")
+
+# ordenamiento con zip
+lista_nueva = ['c', 'a', 'e', 't', 'b','d']
+numeros = [5,6,1,0,3,2]
+mezcla = zip(lista_nueva, numeros)
+# sin ordenar
+print(tuple(mezcla))
+# ordenando
+print(sorted(zip(lista_nueva, numeros)))
+
+# crear un diccionario con zip y dos iterables
+llaves = ["nombre", "apellido", "edad"]
+valores = ["Juan", "Perez", "23"]
+diccionario = dict(zip(llaves, valores))
+print(diccionario)   
+
+#actualizar un elemento de un diccionario
+llave = ["edad"]
+nueva_edad = [40]
+diccionario.update(zip(llave, nueva_edad))
+print(diccionario)
