510f3e396ac734df8d73f5196995b8fb82656c86
Juan <duran85juan@gmail.com>
2021-12-02 22:56:24 -0300

    trabajando con generadores

---
+++ ejercicios_basicos/generadores/expresion_generadora.py
@@ -0,0 +1,33 @@
+import math
+
+# Expresion generadora (es un generador anonimo)
+
+multiplicar = (valor*valor for valor in range(6))
+print(type(multiplicar))
+print(next(multiplicar))
+print(next(multiplicar))
+print(next(multiplicar))
+print(next(multiplicar))
+print(next(multiplicar))
+print(next(multiplicar))
+
+
+# tambien se puede pasar una funcion generadora a una funcion sin parentisis
+suma = sum(valor*valor for valor in range(6))
+print(type(suma))
+print(suma)
+
+# Usando lista con generador
+lista = ['Juan', 'Pedro', 'Maria', 'Ana']
+generador = (nombre for nombre in lista)
+print(type(generador))
+print(next(generador))
+print(next(generador))
+print(next(generador))
+print(next(generador))
+
+# crear un string a partir de un generador que es creado a partir de una lista
+lista = ['Juan', 'Pedro', 'Maria', 'Ana']
+
+
+
---
+++ ejercicios_basicos/generadores/generadores.py
@@ -0,0 +1,65 @@
+''' Generadores '''
+# Los generadores en python son una funcion especial que permiten regresar una secuancia de valores pero no todos al mismo tiempo con la palabra reservada yield, suspendiendo la ejecucion de la funcion hasta que se llame a la funcion next(), por lo que no se usa la palabra return.
+
+def generador():
+    ''' Generador de numeros '''
+    for i in range(10):
+        yield i
+      
+# consumir el generador a demanda        
+resultado = generador()
+# con cada llamada se consume un valor
+print(next(resultado))
+print(next(resultado))
+print(next(resultado))
+print(next(resultado))
+print(next(resultado))
+
+print(''.center(20, '-'))
+
+def generador2():
+    ''' Generador de numeros '''
+    for i in range(10):
+        yield i
+
+for item in generador2():
+    print(item)
+    
+    
+# generador de numeros del 1 al 5
+def generador_numeros():
+    for numero in range(1,6):
+        yield numero
+        print(f'Generando numero: {numero}')
+        
+for item in generador_numeros():
+    print(item)
+    
+resultado2 = generador_numeros()
+try:   
+    print(next(resultado2))
+    print(next(resultado2))
+    print(next(resultado2))
+    print(next(resultado2))
+    print(next(resultado2))
+    print(next(resultado2))
+    print(next(resultado2))
+    print(next(resultado2))
+    print(next(resultado2))
+    print(next(resultado2))
+except StopIteration:
+    print('Se termino el generador')
+    
+print(''.center(20, '-'))
+
+# usando ciclo while
+
+resultado3 = generador_numeros()
+while True:
+    try:
+        valor = next(resultado3)
+        print(valor)
+    except StopIteration:
+        print('Se termino el generador')
+        break
+
