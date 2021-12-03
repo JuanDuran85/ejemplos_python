8e90bdc4bcd4164fd4769507d762bab289f82007
Juan <duran85juan@gmail.com>
2021-12-03 17:18:48 -0300

    trabajando con generadores

--- ejercicios_basicos/generadores/expresion_generadora.py
+++ ejercicios_basicos/generadores/expresion_generadora.py
@@ -28,6 +28,21 @@ print(next(generador))
 
 # crear un string a partir de un generador que es creado a partir de una lista
 lista = ['Juan', 'Pedro', 'Maria', 'Ana']
+contador = 0
 
+# definir funcion que permita incremenentar la variable contador
+def incrementar_contador():
+    global contador
+    contador += 1
+    return contador
 
+# crear expresion generadora
+# la primera parte es el yield, la segunda pate es el for, entre parentesis
 
+generador = (f'{incrementar_contador()}.- {nombre}' for nombre in lista)
+
+# crear una lista a partir del generador
+lista = list(generador)
+print(lista)
+cadena = ', '.join(lista)
+print(cadena)
---
+++ ejercicios_basicos/generadores/list_comprehensions.py
@@ -0,0 +1,62 @@
+# creamos una lista usando el range
+numeros_listas = range(10)
+lista_pares = []
+
+# creamos una nueva lista con los valores pares multiplicados ṕor si mismo desde la lista anterior
+for numero in numeros_listas:
+    if numero % 2 == 0:
+        lista_pares.append(numero*numero)
+        print(numero)
+
+print(lista_pares)
+
+# Ahora trabajando el ejemplo anterior pero con list comprehensions
+''' 
+sintaxis de list comprehensions: 
+    [<expresion> for <elemento> in <lista> if <condicion>]
+la condicion if es opcional
+'''
+numeros_listas_nueva = range(10)
+print(numeros_listas_nueva)
+lista_pares_nueva = []
+lista_pares_nueva = [
+    numero*numero for numero in numeros_listas_nueva if numero % 2 == 0]
+print(lista_pares_nueva)
+
+# se puede trabajar cuando existe dos condicion
+lista = range(100)
+lista_nueva = []
+lista_nueva = [numero for numero in lista if numero %
+               2 == 0 if numero % 3 == 0]
+print(lista_nueva)
+
+# agreando if else
+numeros_pares = []
+numeros_impares = []
+[numeros_pares.append(numero) if numero % 2 == 0 else numeros_impares.append(
+    numero) for numero in range(10)]
+print(numeros_pares)
+print(numeros_impares)
+
+# lista de lista (matriz) para convertir en una sola lista
+lista_de_listas = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# convertir la lista de lista (matriz) en una sola
+lista_simple = [valor for sublista in lista_de_listas for valor in sublista]
+print(lista_simple)
+
+# creando una lista de numeros pares per de una matriz
+# primero sin list comprehensions. Utilizando ciclos for anidados
+matriz3_3 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+pares = []
+for sublista in matriz3_3:
+    for valor in sublista:
+        if valor % 2 == 0:
+            pares.append(valor)
+
+print(pares)
+
+# ahora el ejemplo anterior usando list comprehensions
+matriz3_3 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+pares = [valor for sublista in matriz3_3 for valor in sublista if valor%2 == 0]
+print(pares)
+
