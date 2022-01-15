7835b9d0c05f0eb475fb4072cc0e77ac136604e2
Juan <duran85juan@gmail.com>
2022-01-15 19:17:44 +0100

    trabajando con python, django y postgresql

--- django_proyects/proyecto1/sap/personas/views.py
+++ django_proyects/proyecto1/sap/personas/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from personas.models import Persona
+
 # Create your views here.
+def detalle_persona(request, id):
+    persona_id = Persona.objects.get(pk=id)
+    return render(request, 'detalle_persona.html', {'persona':persona_id})
--- django_proyects/proyecto1/sap/webapp/views.py
+++ django_proyects/proyecto1/sap/webapp/views.py
@@ -1,13 +1,12 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+from personas.models import Persona
+
 # Create your views here.
 
 def bienvenidos(request):
-    return HttpResponse("Bienvenidos a SAP")
-
-def despedida(request):
-    return HttpResponse("Hasta luego")
-
-def contacto(request):
-    return HttpResponse("Contacto")
+    # se utiliza las clases del modelo para trear los objetos
+    numero_personas = Persona.objects.count()
+    personas = Persona.objects.all()
+    return render(request, 'bienvenido.html', {'numero_personas':numero_personas, 'personas':personas})
