9753cfba473466b96b9023f15295fa2407772d2a
Juan <duran85juan@gmail.com>
2022-01-15 22:38:02 +0100

    agregando personas con django a la base de datos

--- django_proyects/proyecto1/sap/personas/views.py
+++ django_proyects/proyecto1/sap/personas/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.forms import modelform_factory
+from django.shortcuts import get_object_or_404, redirect, render
 
 from personas.models import Persona
 
 # Create your views here.
 def detalle_persona(request, id):
-    persona_id = Persona.objects.get(pk=id)
+    # al utilizar la funcion get retorna un error si no existe el registro
+    # persona_id = Persona.objects.get(pk=id)
+    
+    persona_id = get_object_or_404(Persona, pk=id)
+    
     return render(request, 'detalle_persona.html', {'persona':persona_id})
+
+# clase para generar el formulario con la informacion de a base de datos
+PersonaForm = modelform_factory(Persona, exclude=[])
+
+def nueva_persona(request):
+    if request.method == 'POST':
+        forma_persona = PersonaForm(request.POST)
+        if forma_persona.is_valid():
+            forma_persona.save()
+            return redirect('index')
+    else:
+        forma_persona =PersonaForm()
+    
+    return render(request, 'nueva_persona.html', {'formaPersona':forma_persona})
--- django_proyects/proyecto1/sap/sap/urls.py
+++ django_proyects/proyecto1/sap/sap/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from personas.views import detalle_persona
+from personas.views import detalle_persona, nueva_persona
 
 from webapp.views import bienvenidos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', bienvenidos),
+    path('', bienvenidos, name='index'),
     path('detalle_persona/<int:id>', detalle_persona),
+    path('nueva_persona', nueva_persona),
 ]
