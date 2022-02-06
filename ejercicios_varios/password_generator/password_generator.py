401c85e17533bf833c2ea92eb40eda56a6ad439a
Juan <duran85juan@gmail.com>
2022-02-05 21:45:29 -0300

    new password generator

---
+++ ejercicios_varios/password_generator/password_generator.py
@@ -0,0 +1,32 @@
+import string
+from random import randint, sample
+
+c_lower = string.ascii_lowercase
+print(f"{c_lower = }")
+
+c_upper = string.ascii_uppercase
+print(f"{c_upper = }")
+
+c_digits = string.digits
+print(f"{c_digits = }")
+
+c_special = string.punctuation
+print(f"{c_special = }")
+
+cl = sample(c_lower, 4)
+print(f"{cl = }")
+
+cu = sample(c_upper, 4)
+print(f"{cu = }")
+
+cd = sample(c_digits, 2)
+print(f"{cd = }")
+
+cs = sample(c_special, 2)
+print(f"{cs = }")
+
+c = cl + cu + cd + cs
+print(f"{c = }")
+
+password_new = "".join(sample(c,12))
+print(f"{password_new = }")
