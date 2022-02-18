9e4469a28e8e37887909ec6102d944f459e2ed52
Juan <duran85juan@gmail.com>
2022-02-18 13:14:47 -0300

    trabajando con librerias

---
+++ ejercicios_basicos/working_libreries/libraries_time.py
@@ -0,0 +1,13 @@
+"""[summary]
+
+    Using sleep to pause the execution of a program
+
+"""
+
+from time import sleep
+
+lista = [3,4,8,2,1,9,7,5,6]
+
+for item in lista:
+    print(item)
+    sleep(5)
