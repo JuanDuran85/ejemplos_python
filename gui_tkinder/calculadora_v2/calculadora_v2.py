4745a4ab9c8070b41a5fcdd8019b01728959354c
Juan <duran85juan@gmail.com>
2022-03-22 22:56:06 -0300

    trabajando con tkinder

--- gui_tkinder/calculadora_v2/calculadora_v2.py
+++ gui_tkinder/calculadora_v2/calculadora_v2.py
@@ -6,6 +6,11 @@
 
 from tkinter import Tk, Label, Entry, Button, Frame, StringVar, IntVar, END, W, E, S, N, messagebox
 
+# Funciones
+# funcion reset del boton
+def reset() -> None:
+    print("reset")
+
 ventana: Tk = Tk()
 ventana.title("Calculadora")
 ventana.geometry("410x400")
@@ -16,11 +21,6 @@ ventana.config(bg="gray")
 
 pantalla: Entry = Entry(ventana, font=("Arial", 20, "bold"), borderwidth=2, )
 pantalla.grid(row=0, column=0, columnspan=3, padx=5, pady=10)
-
-# funcion reset del boton
-def reset() -> None:
-    print("reset")
-
 
 # boton reiniciar la operacion
 boton_reset: Button = Button(ventana, text="AC", width=8, height=2, font=("Arial", 10, "bold"), command=lambda: reset())
