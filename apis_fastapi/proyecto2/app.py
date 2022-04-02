4c7b68b8f154c1b425944e2eac715a17367e5e37
Juan <duran85juan@gmail.com>
2022-04-02 16:28:48 -0300

    working with fastapi

--- apis_fastapi/proyecto2/app.py
+++ apis_fastapi/proyecto2/app.py
@@ -6,10 +6,14 @@
 
 """
 
-from fastapi import FastAPI
-import uvicorn
+from fastapi import FastAPI, Response
+from pydantic import BaseModel
 
 app: FastAPI = FastAPI()
+
+class Product(BaseModel):
+    name: str
+    price: float
 
 products: list = [
     {"id": 1, "name": "Foo", "price": 100},
@@ -26,9 +30,50 @@ def index() -> dict:
 def all_products() -> list:
     return products
 
+@app.get("/products/search")
+def product_search(name, response: Response) -> list:
+    found_products = [product for product in products if name.lower() in product['name'].lower()]
+    if len(found_products) == 0:
+        response.status_code = 404
+        return [{"message": "Product not found by Name"}]
+
+    return found_products
+
 @app.get("/products/{id}")
-def product_by_id(id: int) -> dict:
+def product_by_id(id: int, response: Response) -> dict:
     for product in products:
         if product['id'] == id:
             return product
+    response.status_code = 404
     return {"message": "Product not found"}
+
+@app.post('/products')
+def add_product(new_product: Product, response: Response) -> list:
+    product_enter = new_product.dict()
+    product_enter['id'] = len(products) + 1
+    products.append(product_enter)
+    response.status_code = 201
+    return products
+
+@app.put('/products/{id}')
+def put_product(id: int, edited_product: Product, response: Response) -> list:
+    for product in products:
+        if product['id'] == id:
+            product['name'] = edited_product.name
+            product['price'] = edited_product.price
+            response.status_code = 200
+            return product
+
+    response.status_code = 404
+    return [{"message": "Product not found"}]
+
+@app.delete('/products/{id}')
+def deleted_product(id: int, response: Response) -> list:
+    for product in products:
+        if product['id'] == id:
+            products.remove(product)
+            response.status_code = 200
+            return products
+
+    response.status_code = 404
+    return [{"message": "Product not found"}]
