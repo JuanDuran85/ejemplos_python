b4190027d1476138657bd68a686ace1a46dd62e4
Juan <duran85juan@gmail.com>
2022-05-02 16:04:28 -0400

    context managert

--- tricks_tips/context_manager.py
+++ tricks_tips/context_manager.py
@@ -43,6 +43,45 @@ def manejador_archivo(nombre_archivo: str) -> Generator[TextIOWrapper, None, Non
         archivo.close()
         print("Se cierra el archivo")
             
+#-------------------------------------------------------------------------------- 
+
+# ejercicio de identador
+
+class Identador:
+    def __init__(self) -> None:
+        self.nivel: int = 0
+
+    def __enter__(self):
+        self.nivel += 1
+        print(f"Nivel: {self.nivel}")
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        self.nivel -= 1
+    
+    def imprimir(self, texto: str):
+        print('  '*self.nivel + texto)
+        
+# ejercicio de identador con la libreria contextlib
+class IdentadorDos:
+    def __init__(self) -> None:
+        self.nivel: int = 0
+        
+    @contextmanager
+    def identador(self) -> Generator[None, None, None]:
+        try:
+            self.nivel += 1
+            yield self
+        except Exception as e:
+            print(e)
+            raise e
+        finally:
+            self.nivel -= 1
+        
+    def imprimir(self, texto: str) -> None:
+        print('   '*self.nivel + texto)
+        
+              
 #--------------------------------------------------------------------------------            
 if __name__ == "__main__":
     # prueba opcion 1
@@ -55,3 +94,21 @@ if __name__ == "__main__":
         archivo_dos.write("Nuevo mensaje desde manejador_archivo")
         archivo_dos.write("\nNuevo mensaje desde manejador_archivo")
         print("Mensaje escrito en el archivo")
+        
+    # prueba identador
+    with Identador() as identador:
+        identador.imprimir("Nuevo texto primer nivel")
+        with identador:
+            identador.imprimir("Nuevo texto segundo nivel")
+            with identador:
+                identador.imprimir("Nuevo texto tercer nivel")
+                
+    # prueba identador con la libreria contextlib
+    print("\nPrueba identador con la libreria contextlib")
+    objetos_iterador: IdentadorDos = IdentadorDos()
+    with objetos_iterador.identador():
+        objetos_iterador.imprimir("Mensaje en el primer nivel")
+        with objetos_iterador.identador():
+            objetos_iterador.imprimir("Mensaje en el segundo nivel")
+            with objetos_iterador.identador():
+                objetos_iterador.imprimir("Mensaje en el tercer nivel")
