3025b61a5e02c5aa9b55d365f464d32fe9fb4ed7
Juan <duran85juan@gmail.com>
2022-05-26 19:14:03 -0400

    archivos csv

--- ejercicios_basicos/manejo_archivos_hojas_pdf_csv/ejemplo3/lectura_archivo.py
+++ ejercicios_basicos/manejo_archivos_hojas_pdf_csv/ejemplo3/lectura_archivo.py
@@ -1,6 +1,9 @@
 # trabajando con archivos - lectura
+
+path_text_local: str = "archivos.txt"
+
 try:
-    archivo = open('archivos.txt', 'r', encoding='utf8')
+    archivo = open(path_text_local, 'r', encoding='utf8')
     # para leer linea por linea
     for linea in archivo:
         print(linea)
@@ -8,21 +11,21 @@ except Exception as e:
     print(e)
 
 try:
-    archivo = open('archivos.txt', 'r', encoding='utf8')
+    archivo = open(path_text_local, 'r', encoding='utf8')
     # para leer todas las lineas en el archivo
     print(archivo.readlines())
 except Exception as e:
     print(e)
 
 try:
-    archivo = open('archivos.txt', 'r', encoding='utf8')
+    archivo = open(path_text_local, 'r', encoding='utf8')
     # para leer una linea en especifico
     print(archivo.readlines()[2])
 except Exception as e:
     print(e)
 
 try:
-    archivo = open('archivos.txt', 'r', encoding='utf8')
+    archivo = open(path_text_local, 'r', encoding='utf8')
     # creando copia del archivo. 
     # "a" es para anexar información al archivo (no borra información)
     # "w" es para sobreescribir el archivo
