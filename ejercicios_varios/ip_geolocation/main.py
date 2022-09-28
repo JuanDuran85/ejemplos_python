97a9029e2cd12e8ef827e60e06159dbe6a88b522
Juan <duran85juan@gmail.com>
2022-09-28 17:53:26 -0300

    ip location

---
+++ ejercicios_varios/ip_geolocation/main.py
@@ -0,0 +1,19 @@
+import httpx, json, asyncio, os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+async def get_ip_location(ip_to_find:str):
+    try:
+        url_base: str = os.getenv("URL")
+        api_key: str =os.getenv("API_KEY")
+        ip_address: str = f"ip_address={ip_to_find if len(ip_to_find)>1 else ''}"
+        async with httpx.AsyncClient() as client:
+            response_api: httpx.Response = await client.get(f"{url_base}?api_key={api_key}&{ip_address}")
+            print(response_api.text)
+    except Exception as e:
+        print(f"ERROR_GET_DATA: {str(e)}")
+    
+if __name__ =='__main__':
+    ip_to_find: str = ""
+    asyncio.run(get_ip_location(ip_to_find))
---
+++ ejercicios_varios/twitter_app/main.py
@@ -0,0 +1,12 @@
+import json, httpx, asyncio, os
+from dotenv import load_dotenv
+
+async def main():
+    try:
+        pass
+    except Exception as e:
+        raise e
+    
+    
+if __name__ == '__main__':
+    asyncio.run(main)
