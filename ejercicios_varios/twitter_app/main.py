e251db7c375e1a5124ce69ddb3ccc45e4ecabe88
Juan <duran85juan@gmail.com>
2022-09-29 01:46:40 -0300

    twitter data

--- ejercicios_varios/twitter_app/main.py
+++ ejercicios_varios/twitter_app/main.py
@@ -1,12 +1,28 @@
 import json, httpx, asyncio, os
 from dotenv import load_dotenv
 
-async def main():
+load_dotenv()
+
+headers = {
+'Authorization': f"Bearer {os.getenv('BEARER_TOKEN')}"
+}
+url: str = os.getenv('URL_BASE')
+
+async def main(name_user_in: str):
     try:
-        pass
+        url_final: str = f"{url}users/by?usernames={name_user_in}&user.fields=created_at&expansions=pinned_tweet_id&tweet.fields=author_id,created_at"
+        async with httpx.AsyncClient() as client:
+            response_api: httpx.Response = await client.get(url_final, headers=headers)
+            print(response_api)
     except Exception as e:
-        raise e
+        print(f"ERROR_API: {str(e)}")
     
+async def get_data_from_api(client,url_final):
+    try:
+        return await client.get(url_final, headers=headers)
+    except Exception as e:
+        print(f"ERROR: {str(e)}")
     
 if __name__ == '__main__':
-    asyncio.run(main)
+    name_user: str = input("Ingrese el nombre del usuario a buscar: ")
+    asyncio.run(main(name_user))
