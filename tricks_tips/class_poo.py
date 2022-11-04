7f87035eb7fa41ac59632e23062fce7d53864ac6
Juan <duran85juan@gmail.com>
2022-11-03 22:30:19 -0300

    using libraries

--- tricks_tips/class_poo.py
+++ tricks_tips/class_poo.py
@@ -201,29 +201,4 @@ def length_string(text_in: str) -> bool:
 try:
     length_string("Mesds")    
 except ExceptionBaseClase as e:
-    print(f"{type(e).__name__}, linea: {e.__traceback__.tb_lineno} en {__file__}, siendo el error: {e}")
-    
-# ----------------------------------------------------------------------------------------------
-# Use the simplest possible argumennts for your methods. Becouse you can pass them directly from your entry points (API, Celery jobs, etc), no need form unnecessarily loading of complex objects.
-
-from dataclasses import dataclass
-
-@dataclass
-class User:
-    id: int
-    username: str
-    
-@dataclass
-class Ticket:
-    id: int
-    user_id: int
-    title: str
-    
-USERS: list[User] = [User(id=1, username="ejemplo1234")]
-TICKETS: list[Ticket] = [Ticket(id=1, user_id=1, title="Ejemplo de titulo")]
-
-def list_user_tickets(user_id: int)-> list[Ticket]:
-    return [ticket for ticket in TICKETS if ticket.user_id == user_id]
-
-result:list[Ticket] = list_user_tickets(USERS[0].id)
-print(result)
+    print(f"{type(e).__name__}, linea: {e.__traceback__.tb_lineno} en {__file__}, siendo el error: {e}") # type: ignore
