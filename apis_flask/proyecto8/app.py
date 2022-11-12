f11a51dec2a1345bb17c31e5b1207a025a1af04c
Juan <duran85juan@gmail.com>
2022-11-12 16:20:56 -0300

    working with flash

--- apis_flask/proyecto8/app.py
+++ apis_flask/proyecto8/app.py
@@ -4,7 +4,7 @@
 
 """
 
-from flask import Flask
+from flask import Flask, url_for, render_template
 
 app: Flask = Flask(__name__)
 
@@ -21,6 +21,32 @@ def hello_func() -> str:
 @app.route('/test/<input_one>/<lang>')
 def test(input_one: str = "hola...",lang: str = "En") -> str:
     return f'Mensaje: {input_one} + {lang}...'
+
+@app.route('/html')
+@app.route('/html/<name>')
+def primer_html(name: str = "Juan") -> str:
+    return '''
+    <html>
+        <body>
+            <h1>Primer Html</h1>
+            <p>Parrafo</p>
+            <h3>%s</h3>
+        </body>
+    </html>
+    ''' %name
+    
+@app.route('/static_file')
+def get_static_file():
+    return "<img src='./static/img/index.png'>"
+
+@app.route('/static_file_ii')
+def get_static_file_ii():
+    return f"<img src='{url_for('static',filename='img/index.png')}'>"
+
+@app.route('/index_html')
+@app.route('/index_html/<value>')
+def index_html(value = None) -> str:
+    return render_template('index.html', value=value)
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
