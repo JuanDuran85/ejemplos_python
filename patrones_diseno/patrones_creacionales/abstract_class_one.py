97a1d4bff0b7285c25409c0c7afdbe85b292c799
Juan <duran85juan@gmail.com>
2022-11-25 23:34:44 -0300

    abstract class

--- patrones_diseno/patrones_creacionales/abstract_class_one.py
+++ patrones_diseno/patrones_creacionales/abstract_class_one.py
@@ -15,23 +15,68 @@ If you inherit from the Animal class but don't implement the abstract methods, y
 
 class Animal(ABC):
 
+    @property
+    def food_eaten(self):
+        return self._food
+
+    @food_eaten.setter
+    def food_eaten(self, food):
+        if food in self.diet:
+            self._food = food
+        else:
+            raise ValueError(f"you can't feed this animal with {food}")
+    
+    @property
     @abstractmethod
-    def feed(self):
-        pass
+    def diet(self): ...
+    
+    @abstractmethod
+    def feed(self): ...
+
+    def do_action(self, action): ...
 
 class Lion(Animal):
-    def feed(self):
-        return "feed the lion"
+    @property
+    def diet(self):
+        return ["antelope","sheep","buffaloe"]
+
+    def feed(self, time="1300"):
+        return f"feeding the lion with {self._food} at {time}"
+
+    def do_action(self, action, extra_time="900"):
+        return f"{action} a Lion, at {extra_time}"
 
 class Panda(Animal):
-    def feed(self):
-        return "feed the Panda"
+    @property
+    def diet(self):
+        return ["bambu"]
+
+    def feed(self, time="1300"):
+        return f"feeding the panda with {self._food} at {time}"
+
+    def do_action(self, action, extra_time="900"):
+        return f"{action} a Panda, at {extra_time}"
 
 class Snake(Animal):
-    def feed(self):
-        return "feed the Snake"
+    @property
+    def diet(self):
+        return ["frog","rabbit","mouse"]
+
+    def feed(self, time="1300"):
+        return f"feeding the snake with {self._food} at {time}"
+
+    def do_action(self, action, extra_time="900"):
+        return f"{action} a Snake, at {extra_time}"
 
 # creating and feeding animals
 zoo = [Lion(), Panda(), Snake(),]
-for animal in zoo:
+food = ["antelope","bambu","frog"]
+for animal,meal in zip(zoo,food):
+    animal.food_eaten = meal 
     print(animal.feed())
+    print(animal.do_action(action="feeding",extra_time="9:00"))
+
+# creating and feeding animals with property, setter and abstract methods
+lion = Lion()
+lion.food_eaten = "buffaloe"
+print(lion.feed(time="1850"))
