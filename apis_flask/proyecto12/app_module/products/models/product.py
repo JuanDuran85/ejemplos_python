baf17027e1199897d75cf2254d19054ed9c663cb
Juan <duran85juan@gmail.com>
2022-11-28 16:46:54 -0300

    working with flask and sql

--- apis_flask/proyecto12/app_module/products/models/product.py
+++ apis_flask/proyecto12/app_module/products/models/product.py
@@ -1,13 +1,22 @@
 from app_module import db
 
 class Product(db.Model):  # type: ignore
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "products"
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True, db_index=True)
     name = db.Column(db.String(255))
     price = db.Column(db.Float)
     
     def __init__(self,name,price):
         self.name = name
         self.price = price
+        
+    
+    def __str__(self):
+        return (
+            f'Id: {self.id}, '
+            f'Nombre: {self.name}, ' 
+            f'Apellido: {self.price}, '
+        )
         
     def __repr__(self):
         return f"<Product {self.name}>"
