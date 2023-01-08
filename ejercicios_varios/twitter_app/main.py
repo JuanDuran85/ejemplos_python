1324d92b8fd7dfa1f96562139ce4fd1031d05b09
Juan <duran85juan@gmail.com>
2023-01-08 18:56:20 -0300

    twitter api

--- ejercicios_varios/twitter_app/main.py
+++ ejercicios_varios/twitter_app/main.py
@@ -37,11 +37,9 @@ async def get_follow_users(id_user_in: str, mode_find: str = "followers", token_
         _type_: _description_
     """
     try:
-        url_with_all: str = ""
-        url_final: str = f"{url}users/{id_user_in}/{mode_find}?max_results=1000&user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,verified_type,withheld&tweet.fields=attachments,author_id,context_annotations,conversation_id,created_at,edit_controls,edit_history_tweet_ids,entities,geo,id,in_reply_to_user_id,lang,non_public_metrics,organic_metrics,possibly_sensitive,promoted_metrics,public_metrics,referenced_tweets,reply_settings,source,text,withheld"
-        url_with_all = f"{url_final}&{token_in}" if token_in != "" else url_final
+        url_final: str = f"{url}users/{id_user_in}/{mode_find}?max_results=1000{f'&pagination_token={token_in}' if token_in != '' else ''}&user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,verified_type,withheld&tweet.fields=attachments,author_id,context_annotations,conversation_id,created_at,edit_controls,edit_history_tweet_ids,entities,geo,id,in_reply_to_user_id,lang,non_public_metrics,organic_metrics,possibly_sensitive,promoted_metrics,public_metrics,referenced_tweets,reply_settings,source,text,withheld"
         async with httpx.AsyncClient() as client:
-            response_api: httpx.Response = await get_data_from_api(client, url_with_all) # type: ignore            
+            response_api: httpx.Response = await get_data_from_api(client, url_final) # type: ignore            
             return response_api.json()
     except Exception as e:
         print(f"ERROR_API: {str(e)}")
@@ -65,24 +63,30 @@ async def get_data_from_api(client,url_final: str):
 if __name__ == '__main__':
     name_user: str = input("Ingrese el nombre del usuario a buscar: ")
     info_user: dict | None = asyncio.run(main(name_user))
-    
-    if not info_user.get("errors") : # type: ignore
+
+    if not info_user.get("errors"): # type: ignore
         id_user: str = info_user["data"][0]["id"] # type: ignore
-        pagination_active: str | None = "token"
-        while pagination_active:
-            followers: dict | None = asyncio.run(get_follow_users(id_user,"followers",""))
-            if followers.get("meta"): # type: ignore
-                pagination_active = followers["meta"]["next_token"] # type: ignore
-            else:
-                pagination_active = None
+        pagination_active: str = ""
+        data_from_followers: list = []
+        while True:
+            followers: dict | None = asyncio.run(get_follow_users(id_user,"followers",pagination_active))
+            if not followers.get("meta"): # type: ignore
+                data_from_followers.append(followers)
+                break
+            pagination_active = followers["meta"]["next_token"] if followers["meta"].get("next_token") else None # type: ignore
+            data_from_followers.append(followers["data"]) # type: ignore
+            if pagination_active is None:
+                break
+        
+        print(f"{data_from_followers = }")
         
         following: dict | None = asyncio.run(get_follow_users(id_user,"following"))
         print(following)
-        
+
         data_following: list[dict] = following["data"] # type: ignore
-        #print(json.dumps(followers, indent=4, sort_keys=True))
-        #print(json.dumps(following, indent=4, sort_keys=True))
-        
+                    #print(json.dumps(followers, indent=4, sort_keys=True))
+                    #print(json.dumps(following, indent=4, sort_keys=True))
+
     else:
         print("El usuario no existe")
                     
