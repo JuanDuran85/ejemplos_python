5d87cdc87768d7de72edd87577c9271849ca5c52
JuanDuran85 <duran85juan@gmail.com>
2023-10-22 00:15:16 -0300

    examples

--- ejercicios_basicos/excepciones/exepciones.py
+++ ejercicios_basicos/excepciones/exepciones.py
@@ -1,22 +1,31 @@
 try:
-    fichero = open("ejemplo.txt","r");
-    lineas = fichero.readlines();
-    fichero.close();
-    print(lineas);
+    fichero = open("ejemplo.txt", "r")
+    lineas = fichero.readlines()
+    fichero.close()
+    print(lineas)
 except Exception:
-    print("Error al abrir el fichero");
+    print("Error al abrir el fichero")
 
 print("Fin del programa")
 
 # otra forma implementando else
 
 try:
-    fichero = open("ejemplo.txt","r");
+    fichero = open("ejemplo.txt", "r")
 except Exception:
-    print("Error al abrir el fichero");
+    print("Error al abrir el fichero")
 else:
-    lineas = fichero.readlines();
-    fichero.close();
-    print(lineas);
+    lineas = fichero.readlines()
+    fichero.close()
+    print(lineas)
 
 print("Fin del programa")
+
+
+class ComH():
+    def connect(self, user1, user2):
+        try:
+            if user1.name == user2.name:
+                raise Exception("{user1.name} is already")
+        except Exception:
+            print('An exception occurred')
