f9fc2bf83c0deac13105d4898e7668be5fa1fd42
JuanDuran85 <duran85juan@gmail.com>
2025-01-22 16:39:21 -0300

    refactor example8.py to remove unnecessary semicolons and improve code readability

--- ejercicios_basicos/listas_tuplas_diccionarios/ejemplo8.py
+++ ejercicios_basicos/listas_tuplas_diccionarios/ejemplo8.py
@@ -1,23 +1,23 @@
-numero = 5;
-texto = "Mensaje nuevo";
+numero = 5
+texto = "Mensaje nuevo"
 
-print(f"El valor del numero en la variable es {numero}, mientras que el valor del texo es: {texto}");
+print(f"El valor del numero en la variable es {numero}, mientras que el valor del texo es: {texto}")
 
-numero2 = 34;
+numero2 = 34
 
-print(f"La suma de dos variables es: {numero + numero2}");
+print(f"La suma de dos variables es: {numero + numero2}")
 
 # Listas
 
-lista = ["Python", "Angular", "Vue", "Nest", "Node"];
+lista = ["Python", "Angular", "Vue", "Nest", "Node"]
 
-lista.append("Django");
-lista.insert(4, "Express");
+lista.append("Django")
+lista.insert(4, "Express")
 
 print(lista);
 
 # tuplas: son inmutables. Las tuplas cuando tienen un dato deben terminar con la coma y el espacio
-tupla1 = ("Python", "Angular", "Vue", "Nest", "Node");
+tupla1 = ("Python", "Angular", "Vue", "Nest", "Node")
 print(tupla1[1]);
 
 # diccionarios
@@ -30,11 +30,11 @@ datos = {
 };
 
 # agregar un dato
-datos["city"] = "Barcelona";
+datos["city"] = "Barcelona"
 # borrar un dato
-del datos["country"];
+del datos["country"]
 # cantidad de claves
-print(len(datos));
+print(len(datos))
 
-print(datos);
-print(datos["name"]);
+print(datos)
+print(datos["name"])
