fa63c8a43eda4bd787d983a45bf634773197c88f
JuanDuran85 <duran85juan@gmail.com>
2025-05-19 17:55:49 -0400

    refactor libraries_re.py and string_conditions_loop_other.py for improved readability and structure

--- ejercicios_basicos/working_libreries/libraries_re.py
+++ ejercicios_basicos/working_libreries/libraries_re.py
@@ -2,7 +2,8 @@
 
     Working with re library
     
-    Regular expression operations library: This module provides regular expression matching operations similar to those found in Perl.
+    Regular expression operations library: This module provides regular 
+    expression matching operations similar to those found in Perl.
 
 """
 
@@ -15,4 +16,27 @@ import re
 for i in dir(__builtins__):
     if re.match('^[A-Z]',i):
         print(i)
+# ----------------------------------------------------------------------------}
+
+# ----------------------------------------------------------------------------
+# Creating a New List By applying Some operations On the Other List
+# ----------------------------------------------------------------------------
+
+# Suppose you have a 2D list containing general information like names, emails, 
+# and mobile numbers for different users. Your task is to extract all the emails
+# provided by Gmail inside a list.'''
+
+users = [
+    ['Elio', 'P2MlS@example.com', '1234567890', 'VE'],
+    ['John', 'GtZMw@example.com', '548585354'],
+    ['Jane', 'jane@me.com', '5482006658', 'SF'],
+    ['Doe', 't9EYh@example.com', '5482006658']
+]
+ # you can use fullmatch to check if the whole string is a valid email
+emails = [item for user in users for item in user if re.findall(r'(\w+@example\.com)', str(item))]
+
+print(emails, "", end="")
+
+
+
 # ----------------------------------------------------------------------------
