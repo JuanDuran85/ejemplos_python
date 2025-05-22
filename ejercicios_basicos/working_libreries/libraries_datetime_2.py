37a4639deea0134d4681c868f4d210d69ef095cb
JuanDuran85 <duran85juan@gmail.com>
2025-05-22 18:04:37 -0400

    Using datetime library with ternary operator

--- ejercicios_basicos/working_libreries/libraries_datetime_2.py
+++ ejercicios_basicos/working_libreries/libraries_datetime_2.py
@@ -14,3 +14,11 @@ print("Using datetime library with timedelta and datetime.now")
 print(datetime.datetime.now().date() + datetime.timedelta(days=3))
 print(datetime.date(2025, 5, 23))
 print(datetime.date(2025, 5, 23) + datetime.timedelta(days=3))
+
+print("--------------------------------------------------------------")
+print("Using datetime library with ternary operator")
+print(str(
+    datetime.datetime.now().date() + datetime.timedelta(days=1)
+) if datetime.datetime.now().weekday() != 4 else str(
+    datetime.datetime.now().date() + datetime.timedelta(days=3)
+))
