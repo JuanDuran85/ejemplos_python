4d5f4073b2c5bc02b523aec15115e32bf8ede91c
JuanDuran85 <duran85juan@gmail.com>
2025-05-24 17:59:37 -0400

    refactor libraries_dataclasses.py for improved documentation and formatting

--- ejercicios_basicos/working_libreries/libraries_dataclasses.py
+++ ejercicios_basicos/working_libreries/libraries_dataclasses.py
@@ -1,34 +1,53 @@
 """
 Using de dataclasses library.
 
-You can use the dataclasses library to implement type anotations
+You can use the dataclasses library to implement type annotations
 
-eq=True: Automatically generates an __eq__ method to compare instances of this class for equality.
+eq=True: Automatically generates an __eq__ method to compare instances of this
+class for equality.
 
-frozen=True: Disables the possibility of modifying the attributes of instances of this class.
-That is, it makes the instances immutable (they cannot be modified after creation), similar to a tuple.
+frozen=True: Disables the possibility of modifying the attributes of
+instances of this class.
+That is, it makes the instances immutable (they cannot be modified
+after creation), similar to a tuple.
 
-order=True: Automatically generates an __lt__ method to compare instances of this class by order.
+order=True: Automatically generates an __lt__ method to compare instances
+of this class by order.
 
 
 """
 
 from dataclasses import dataclass
 
-# ----------------------------------------------------------------------------------------------# ----------------------------------------------------------------------------------------------
-# Use the simplest possible argumennts for your methods. Becouse you can pass them directly from your entry points (API, Celery jobs, etc), no need form unnecessarily loading of complex objects.
+# ----------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------
+# Use the simplest possible arguments for your methods. Because you can pass
+# them directly from your entry points (API, Celery jobs, etc), no need form
+# unnecessarily loading of complex objects.
 # ----------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------
 
 
 @dataclass
 class User:
+    """summary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
     id: int
     username: str
 
 
 @dataclass
 class Ticket:
+    """summary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
     id: int
     user_id: int
     title: str
@@ -39,23 +58,42 @@ TICKETS: list[Ticket] = [Ticket(id=1, user_id=1, title="Ejemplo de titulo")]
 
 
 def list_user_tickets(user_id: int) -> list[Ticket]:
+    """
+    Retrieve a list of tickets associated with a specific user.
+
+    Args:
+        user_id (int): The ID of the user whose tickets are to be retrieved.
+
+    Returns:
+        list[Ticket]: A list of Ticket objects that belong to the
+        specified user.
+    """
+
     return [ticket for ticket in TICKETS if ticket.user_id == user_id]
 
 
 result: list[Ticket] = list_user_tickets(USERS[0].id)
 print(result)
 
-# ----------------------------------------------------------------------------------------------# ----------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------
 
 
-# ----------------------------------------------------------------------------------------------# ----------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------
 #
 # ----------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------
 
-
 @dataclass
 class UserTwo:
+    """
+    Represents a user with a username.
+
+    Attributes:
+        username (str): The username of the user.
+    """
+
     username: str
 
 
