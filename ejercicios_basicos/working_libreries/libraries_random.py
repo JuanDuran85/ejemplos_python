610612a7d99fcccac55478617365f038b5d7aeb9
JuanDuran85 <duran85juan@gmail.com>
2025-05-24 19:48:33 -0400

    Refactor libraries_random.py to improve function documentation and enhance list handling

--- ejercicios_basicos/working_libreries/libraries_random.py
+++ ejercicios_basicos/working_libreries/libraries_random.py
@@ -1,29 +1,71 @@
 """_summary_
 
- Trabajando con la libreria Random
+ Working with random library
 
 """
 
 import random
 
-#-------------------------------------------------------------------------------------
-def cargar() -> list:
-    lista: list = []
-    for _ in range(10):
-        lista.append(random.randint(1, 100))
-    return lista
+print("")
+print("----------------------------------------------------------------------------")
+# ----------------------------------------------------------------------------
+print("using random - randint and shuffle")
+# ----------------------------------------------------------------------------
+print("----------------------------------------------------------------------------")
+print("")
 
-def imprimir(lista: list) -> None:
-    print(lista)
-    
-def mezcla(lista: list) -> list:
-    random.shuffle(lista)        
-    
+
+def list_load_fn() -> list[int]:
+    """
+    List load function.
+
+    This function will return a list of ten random numbers between 1 and 100.
+    """
+
+    list_load_in: list[int] = []
+    for _ in range(10):
+        list_load_in.append(random.randint(1, 100))
+    return list_load_in
+
+
+def print_out(list_in: list) -> None:
+    """
+    Print out function.
+
+    This function will print out the list that is passed in.
+    """
+
+    print(list_in)
+
+
+def mix_fn(list_mix_in: list) -> None:
+    """
+    Shuffle the elements of a list in place.
+
+    Args:
+        list_mix_in (list): The list whose elements are to be shuffled.
+
+    Returns:
+        None: This function returns nothing. It modifies the input list in place.
+    """
+
+    random.shuffle(list_mix_in)
+
+
 if __name__ == '__main__':
-    lista = cargar()
-    print("Lista creada...")
-    imprimir(lista)
-    mezcla(lista)
-    print("Lista mezclada...")
-    imprimir(lista)
-#-------------------------------------------------------------------------------------
+    list_load: list[int] = list_load_fn()
+    print("List Done...")
+    print_out(list_load)
+    mix_fn(list_load)
+    print("Mix List...")
+
+print("")
+print("----------------------------------------------------------------------------")
+# ----------------------------------------------------------------------------
+print("using random - choice to generate a password")
+# ----------------------------------------------------------------------------
+print("----------------------------------------------------------------------------")
+print("")
+
+PASSWORD_RESULT = ''.join([random.choice('abcdefghijklmnopqrstuvwxyz0123456789%^*(-_=+)') for _ in range(10)])
+print(f"Result = {PASSWORD_RESULT}")
