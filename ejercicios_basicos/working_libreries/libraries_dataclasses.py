de55837ff7e7d16ed254e1fee1b55cc52107aef0
JuanDuran85 <duran85juan@gmail.com>
2025-05-24 17:53:19 -0400

    refactor libraries_dataclasses.py for improved documentation and formatting

--- ejercicios_basicos/working_libreries/libraries_dataclasses.py
+++ ejercicios_basicos/working_libreries/libraries_dataclasses.py
@@ -1,18 +1,17 @@
 """
-    Using de dataclasses library.
-    
-    You can use the dataclasses library to implement type anotations
+Using de dataclasses library.
 
-    eq=True: Automatically generates an __eq__ method to compare instances of this class for equality.
+You can use the dataclasses library to implement type anotations
 
-    frozen=True: Disables the possibility of modifying the attributes of instances of this class. 
-    That is, it makes the instances immutable (they cannot be modified after creation), similar to a tuple.
+eq=True: Automatically generates an __eq__ method to compare instances of this class for equality.
 
-    order=True: Automatically generates an __lt__ method to compare instances of this class by order.
+frozen=True: Disables the possibility of modifying the attributes of instances of this class.
+That is, it makes the instances immutable (they cannot be modified after creation), similar to a tuple.
+
+order=True: Automatically generates an __lt__ method to compare instances of this class by order.
 
 
 """
-
 
 from dataclasses import dataclass
 
@@ -26,20 +25,24 @@ from dataclasses import dataclass
 class User:
     id: int
     username: str
-    
+
+
 @dataclass
 class Ticket:
     id: int
     user_id: int
     title: str
-    
+
+
 USERS: list[User] = [User(id=1, username="ejemplo1234")]
 TICKETS: list[Ticket] = [Ticket(id=1, user_id=1, title="Ejemplo de titulo")]
 
-def list_user_tickets(user_id: int)-> list[Ticket]:
+
+def list_user_tickets(user_id: int) -> list[Ticket]:
     return [ticket for ticket in TICKETS if ticket.user_id == user_id]
 
-result:list[Ticket] = list_user_tickets(USERS[0].id)
+
+result: list[Ticket] = list_user_tickets(USERS[0].id)
 print(result)
 
 # ----------------------------------------------------------------------------------------------# ----------------------------------------------------------------------------------------------
@@ -50,13 +53,23 @@ print(result)
 # ----------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------
 
+
 @dataclass
 class UserTwo:
     username: str
-    
-# good way to use a type anotations
+
+
+# good way to use a type annotations
+
+
 def print_users(users: list[UserTwo]) -> None:
+    """Prints all users in the list
+
+    Args:
+        users (list[UserTwo]): List of users to print
+    """
     for one_user in users:
         print(one_user)
-        
+
+
 print_users([UserTwo(username="Usuario 1")])
