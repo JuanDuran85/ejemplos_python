db2e6fb271ab91f022bb014f6f8cf625776f3541
JuanDuran85 <duran85juan@gmail.com>
2025-05-26 17:19:44 -0400

    Refactor Persona.py for improved formatting and consistency in code style

--- ejercicios_basicos/poo/poo1/Persona.py
+++ ejercicios_basicos/poo/poo1/Persona.py
@@ -1,9 +1,11 @@
 # trabajando con clases (POO)
 
 class Ejemplo:
-    pass # la clase no tendra contenido, solo se utiliza para definir una clase
+    pass  # la clase no tendra contenido, solo se utiliza para definir una clase
+
 
 print(type(Ejemplo))
+
 
 class Persona:
     # self es una referencia al objeto que se esta creando
@@ -14,7 +16,8 @@ class Persona:
         self.edad = edad
 
     def imprimir(self):
-        print("Hola, soy una persona, mi nombre es: ",self.nombre, "y mi edad: ",self.edad)
+        print("Hola, soy una persona, mi nombre es: ", self.nombre, "y mi edad: ", self.edad)
+
 
 persona_uno = Persona("Juan", 36)
 print(persona_uno.nombre)
@@ -23,20 +26,20 @@ persona_uno.imprimir()
 
 
 class Automovil:
-# Recuerde programar el constructor de clase que inicializa los 3 atributos
-    def __init__(self,velocidad_actual,marca,estado):
+    # Recuerde programar el constructor de clase que inicializa los 3 atributos
+    def __init__(self, velocidad_actual, marca, estado):
         self.velocidad_actual = velocidad_actual
         self.marca = marca
         self.estado = estado
-        
+
     def aumentar_velocidad(self):
         self.velocidad_actual += 10
-        
+
     def disminuir_velocidad(self):
         self.velocidad_actual -= 6
-        
+
     def encender(self):
         self.estado = True
-        
+
     def apagar(self):
         self.estado = False
