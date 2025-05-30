a9ee33c7b31a9be60a6f3d48f76f85f460089288
JuanDuran85 <duran85juan@gmail.com>
2025-05-30 12:39:51 -0400

    Refactor code for improved formatting and consistency in function definitions and print statements

--- ejercicios_basicos/map_filter_reduce_lambda/ejemp_func_map.py
+++ ejercicios_basicos/map_filter_reduce_lambda/ejemp_func_map.py
@@ -29,75 +29,81 @@ print(list(map(len, words)))
 
 # utilizando una funcion que retorne el doble del numero ingreso proveniente de una lista, implementar la funcion map()
 print('-----------------------------------------------------------')
-numeros_lista: list = [3,5,7]
-print(f"{numeros_lista = }")
+numeros_lista: list = [3, 5, 7]
+print(f"{numeros_lista=}")
 resultado: list = list(map(lambda x: x*2, numeros_lista))
-print(f"{resultado = }")
+print(f"{resultado=}")
 
 # A partir de una lista de numeros, obtenga una lista con los numeros incrementados en 10 utilizando la funcion map()
 print('-----------------------------------------------------------')
-lista_numeroa: list = list(range(1,11))
+lista_numeroa: list = list(range(1, 11))
 resultado_numeros: list = list(map(lambda x: x+10, lista_numeroa))
-print(f"{resultado_numeros = }")
+print(f"{resultado_numeros=}")
 
-#---------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
 print('----------------------------------------')
 print("\r\nPassing multiple Iterators to map() function")
 print("Lambda function should have one parameter for each iterator.")
-iterator_one: list = [1,5,7]
-iterator_two: list = [9,5,3]
-print(f"{iterator_one = }")
-print(f"{iterator_two = }")
-result_map: list = list(map(lambda x,y: x+y, iterator_one, iterator_two))
-print(f"{result_map = }")
+iterator_one: list = [1, 5, 7]
+iterator_two: list = [9, 5, 3]
+print(f"{iterator_one=}")
+print(f"{iterator_two=}")
+result_map: list = list(map(lambda x, y: x+y, iterator_one, iterator_two))
+print(f"{result_map=}")
 
-#---------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
 print('----------------------------------------')
 print("\r\nPassing multiple Iterators with diﬀerent size")
 print("Let's see what happen if we pass multiple iterators with diﬀerent size. In python 2, we got an exception. Therefore, be aware of this case in your code. In python 3, we get a map object with the size of smallest iterator.")
 
-list_one: list = [1,5,7,9,8]
-list_two: list = [9,5,3]
-print(f"{list_one = }")
-print(f"{list_two = }")
+list_one: list = [1, 5, 7, 9, 8]
+list_two: list = [9, 5, 3]
+print(f"{list_one=}")
+print(f"{list_two=}")
 
-result_map: list = list(map(lambda x,y: x+y, list_one, list_two))
-print(f"{ result_map = }")
+result_map: list = list(map(lambda x, y: x+y, list_one, list_two))
+print(f"{result_map=}")
 
 # ----------------------------------------------------------------------------------------
 # Utilizando la funcion map para retornar una lista con los numeros al cuadrado
 
-lista_numeros: list = [4,7,2,8,4,1,8,9,3,-6,-9]
-print(f"{lista_numeros = }")
+lista_numeros: list = [4, 7, 2, 8, 4, 1, 8, 9, 3, -6, -9]
+print(f"{lista_numeros=}")
 result_lista: list = list(map(lambda x: x**2, lista_numeros))
-print(f"{result_lista = }")
+print(f"{result_lista=}")
 
 # ----------------------------------------------------------------------------------------
 # The map() function takes a function and a series of arguments, and makes am iterable of results. It can also work on functions with multiple arguments. But, most python people prefer list comprehensions.
-#-----------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------
 print('---------------------------------------------------------------------------')
 # map makes an iterator object.
+
+
 def square(x: int) -> int:
     return x**2
 
+
 # sourcery skip: assign-if-exp
 result_map_one: list[int] = list(map(square, range(10)))
-print(f"{result_map_one = }")
+print(f"{result_map_one=}")
 
 print('---------------------------------------------------------------------------')
 # map can be use with more than one argument
-def stradd(a: str,b: int)-> str:
+
+
+def stradd(a: str, b: int) -> str:
     return f"{a}{b}"
 
-result_map_two: list[str] = list(map(stradd,"ABCDEFGHIJ",range(10)))
-print(f"{result_map_two = }")
+
+result_map_two: list[str] = list(map(stradd, "ABCDEFGHIJ", range(10)))
+print(f"{result_map_two=}")
 
 print('---------------------------------------------------------------------------')
 # map can take multiple iterables, and it fetches an argument from each iterable
-# in this case, you can verify is each letter appear in the respective word. 
+# in this case, you can verify is each letter appear in the respective word.
 
 words: list[str] = ["texto 1", "texto 1", "texto 1", "texto 1"]
-letters: list[str] = ["a","e","i","o"]
+letters: list[str] = ["a", "e", "i", "o"]
 
-result_letters: list[bool] = list(map(lambda x_l,y_w: x_l in y_w,letters,words))
-print(f"{result_letters = }")
+result_letters: list[bool] = list(map(lambda x_l, y_w: x_l in y_w, letters, words))
+print(f"{result_letters=}")
--- tricks_tips/functions_others.py
+++ tricks_tips/functions_others.py
@@ -19,38 +19,45 @@ print("\n Using functions \n")
 Functions are first-class citizens in Python. They can be passed as arguments to other functions, returned by other functions, and assigned to variables.
 '''
 
-def my_function(x,y):
+
+def my_function(x, y):
     return x + y
 
+
 functions = [my_function]
-print(f"{functions[0](4,2) = } ")
+print(f"{functions[0](4, 2)=} ")
 
 ''' Using functions unpacking'''
 print("\n Using function argument unpacking \n")
 
-def function_one(x,y,z):
+
+def function_one(x, y, z):
     return x + y + z
 
-tuple_values = (3,6,1)
+
+tuple_values = (3, 6, 1)
 dict_values = {
     'x': 3,
     'y': 6,
     'z': 1
 }
-print(f"{tuple_values = }")
-print(f"{dict_values = }")
-print(f"{function_one(*tuple_values) = } ")
-print(f"{function_one(**dict_values) = } ")
+print(f"{tuple_values=}")
+print(f"{dict_values=}")
+print(f"{function_one(*tuple_values)=} ")
+print(f"{function_one(**dict_values)=} ")
 
 '''
     In Python, you can define a function's arguments to be positional-only. In order to do that, you just use a '/' in the argument list. What comes before the '/' is positional-only
 '''
 
 print(f"\r\nArguments Positional-Only\r\n")
-def example_function(a: int,b: int,/) -> int:
+
+
+def example_function(a: int, b: int, /) -> int:
     return a+b
 
-print(example_function(1,4))
+
+print(example_function(1, 4))
 # print(example_function(1,b=4)), In this case, we catch an error, because the arguments in the function are only positional
 
 
@@ -58,7 +65,8 @@ print(example_function(1,4))
     Becouse Python has first-class functions they can be use to emulate switch/case statements
 '''
 
-def dispatch_if(operator: str,x: int, y: int):
+
+def dispatch_if(operator: str, x: int, y: int):
     if operator == 'add':
         return x + y
     elif operator == 'sub':
@@ -69,36 +77,42 @@ def dispatch_if(operator: str,x: int, y: int):
         return x / y
     else:
         return None
-    
-def dispatch_dict(operator: str,x: int, y: int):
+
+
+def dispatch_dict(operator: str, x: int, y: int):
     return {
         'add': lambda: x + y,
         'sub': lambda: x - y,
         'mul': lambda: x * y,
         'div': lambda: x / y,
     }.get(operator, lambda: None)()
-    
-    
-print(f"{ dispatch_if('add',1,2) = } ")
-print(f"{ dispatch_dict('add',1,2) = } ")
+
+
+print(f"{dispatch_if('add', 1, 2)=} ")
+print(f"{dispatch_dict('add', 1, 2)=} ")
 
 # ----------------------------------------------------------------------------------------------
 # Use * in functions to use positional arguments
+
+
 def my_function_two(*, a, b) -> None:
-    print(a,b)
-    
-my_function_two(a=1,b=2)
+    print(a, b)
+
+
+my_function_two(a=1, b=2)
 # my_function_two(1,2) # Error, because the function only needs positional arguments
 
 # -----------------------------------------------------------------------------------------------
 # Use * in functions parameters to use keyword arguments
 # find the smallest number passed as argument in a function
+
+
 def find_smallest_number(*values) -> int:
-    
+
     # using min() function to find the smallest number
-    lower_value: int = min(values) 
-    print(f"{lower_value = }")
-    #-------------------------------------------------
+    lower_value: int = min(values)
+    print(f"{lower_value=}")
+    # -------------------------------------------------
     # using for loop to find the smallest number
     lower_value_two: int = values[0]
     for value in values:
@@ -106,13 +120,16 @@ def find_smallest_number(*values) -> int:
             lower_value_two = value
     return lower_value_two
 
-print(find_smallest_number(1,-2,3,24,5,6,-7,8,9,-10))
+
+print(find_smallest_number(1, -2, 3, 24, 5, 6, -7, 8, 9, -10))
 
 # ------------------------------------------------------------------------------------------------
 # Use ** in functions parameters to use keyword arguments
 
+
 def print_parameters(**values) -> None:
-    for key,value in values.items():
+    for key, value in values.items():
         print(f"{key} -> {value}")
-        
-print_parameters(a=1,b=2,c=3,d=4,e=5)
+
+
+print_parameters(a=1, b=2, c=3, d=4, e=5)
