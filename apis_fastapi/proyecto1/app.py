9b2630c427bec761e30bc2f80225021fdf3c7a85
JuanDuran85 <duran85juan@gmail.com>
2025-06-01 22:06:30 -0400

    Refactor app.py for improved formatting and consistency in function documentation and spacing

--- apis_fastapi/proyecto1/app.py
+++ apis_fastapi/proyecto1/app.py
@@ -13,10 +13,16 @@ import uvicorn
 app: FastAPI = FastAPI()
 
 # allows to create a route
+
+
 @app.get("/home")
 # will be executed each time a user visits the URL path specified by the @app.get() decorator, in this case the "/home" path
 def index() -> dict:
+    """ 
+    This path operation returns a message saying that the API is working
+    """
     return {"message": "ok"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080)
