516c90e33dcfe436387a953fba58cd62fa561587
JuanDuran85 <duran85juan@gmail.com>
2025-06-01 22:11:03 -0400

    Refactor app.py for improved formatting and consistency in function documentation and spacing

--- apis_fastapi/proyecto2/app.py
+++ apis_fastapi/proyecto2/app.py
@@ -6,19 +6,33 @@
 
 """
 
+from enum import Enum
+
 from fastapi import FastAPI, Response
 from pydantic import BaseModel
-from enum import Enum
 
 app: FastAPI = FastAPI()
 
+
 class Product(BaseModel):
+    """_summary_
+
+    Args:
+        BaseModel (_type_): _description_
+    """
     name: str
     price: float
-    
+
+
 class Tags(Enum):
+    """_summary_
+
+    Args:
+        BaseModel (_type_): _description_
+    """
     items = "items"
     users = "users"
+
 
 products: list = [
     {"id": 1, "name": "Foo", "price": 100},
@@ -29,16 +43,40 @@ products: list = [
 
 PRODUCTS_NOT_FOUND: str = "Product not found"
 
+
 @app.get("/")
 def index() -> dict:
+    """
+    This path operation returns a message saying that the API is working
+    """
+    
     return {"message": "ok"}
+
 
 @app.get('/products')
 def all_products() -> list:
+    """
+    This path operation returns a list of all products.
+
+    Returns:
+        list: A list of dictionaries, where each dictionary represents a product with its details.
+    """
+
     return products
+
 
 @app.get("/products/search")
 def product_search(name, response: Response) -> list:
+    """
+    This path operation searches for products by name.
+
+    Args:
+        name (str): The name to search for.
+        response (Response): The response object to modify the status code.
+
+    Returns:
+        list: A list of dictionaries, where each dictionary represents a product that matches the search query.
+    """
     found_products = [product for product in products if name.lower() in product['name'].lower()]
     if len(found_products) == 0:
         response.status_code = 404
@@ -46,13 +84,28 @@ def product_search(name, response: Response) -> list:
 
     return found_products
 
+
 @app.get("/products/{id}")
 def product_by_id(id: int, response: Response) -> dict:
+    """
+    This path operation returns a single product by its ID.
+
+    Args:
+        id (int): The id of the product to search for.
+        response (Response): The response object to modify the status code.
+
+    Returns:
+        dict: A dictionary representing the product if found, otherwise a dictionary with a message indicating that the product was not found by id.
+
+    Raises:
+        HTTPException: If the product is not found by id.
+    """
     for product in products:
         if product['id'] == id:
             return product
     response.status_code = 404
     return {"message": "Product not found by id"}
+
 
 @app.post('/products')
 def add_product(new_product: Product, response: Response) -> list:
@@ -61,6 +114,7 @@ def add_product(new_product: Product, response: Response) -> list:
     products.append(product_enter)
     response.status_code = 201
     return products
+
 
 @app.put('/products/{id}')
 def put_product(id: int, edited_product: Product, response: Response) -> list:
@@ -74,10 +128,11 @@ def put_product(id: int, edited_product: Product, response: Response) -> list:
     response.status_code = 404
     return [{"message": PRODUCTS_NOT_FOUND}]
 
+
 @app.delete('/products/{id}')
-def deleted_product(id: int, response: Response) -> list:
+def deleted_product(id_in: int, response: Response) -> list:
     for product in products:
-        if product['id'] == id:
+        if product['id'] == id_in:
             products.remove(product)
             response.status_code = 200
             return products
@@ -85,10 +140,12 @@ def deleted_product(id: int, response: Response) -> list:
     response.status_code = 404
     return [{"message": PRODUCTS_NOT_FOUND}]
 
-@app.get("/items/",tags=[Tags.items])
-async def get_items() -> list:
-    return ["Python","JavaScript"]
 
-@app.get("/users/",tags=[Tags.users])
+@app.get("/items/", tags=[Tags.items])
+async def get_items() -> list:
+    return ["Python", "JavaScript"]
+
+
+@app.get("/users/", tags=[Tags.users])
 async def read_users() -> list:
-    return ["Jhon","Doe"]
+    return ["Jhon", "Doe"]
