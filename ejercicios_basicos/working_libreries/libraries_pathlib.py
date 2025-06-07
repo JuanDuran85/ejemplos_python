99ab7f05819b5c7e6f2611471448aa16331f0e68
JuanDuran85 <duran85juan@gmail.com>
2025-06-06 20:17:13 -0400

    Refactor libraries_pathlib.py for improved formatting, enhanced print statements, and consistent use of Pathlib

--- ejercicios_basicos/working_libreries/libraries_pathlib.py
+++ ejercicios_basicos/working_libreries/libraries_pathlib.py
@@ -1,13 +1,35 @@
+# !/usr/bin/python3
+# flake8: noqa: E501
+# pylint: disable=line-too-long
+# pylint: disable=C0103
+
 """_summary_
 
     Working with Pathlib.
 
 """
 
-import pathlib
+from pathlib import Path
 
-filename: str = "ejemplo_uno.pdf"
+LINES = "---------------------------------------------------------------------\r"
+filename: str = "example_one.pdf"
 
-file_type_suffix = pathlib.Path(filename).suffix
+print("using Pathlib")
+print(LINES)
+print("using Path to get the suffix")
+file_type_suffix: str = Path(filename).suffix
 print(f"{filename} has the suffix {file_type_suffix}")
+
+print(LINES)
+print("using Path to remove the suffix")
 print(f"Removing: {filename.removesuffix(file_type_suffix)}")
+
+print(LINES)
+print("using Path to create a path")
+base_path: Path = Path("\\home\\work\\project_one")
+file_name = "data.csv"
+
+filepath: Path = base_path / file_name
+print(filepath)
+
+print(LINES)
