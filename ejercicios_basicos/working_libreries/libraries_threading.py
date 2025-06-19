b5cc3c535df0116492a4f358710c6e0784626f63
JuanDuran85 <duran85juan@gmail.com>
2025-06-19 18:11:38 -0400

    Refactor libraries_threading.py for improved formatting and consistency

--- ejercicios_basicos/working_libreries/libraries_threading.py
+++ ejercicios_basicos/working_libreries/libraries_threading.py
@@ -1,3 +1,8 @@
+# !/usr/bin/python3
+# flake8: noqa: E501
+# pylint: disable=line-too-long
+# pylint: disable=C0103
+
 """_summary_
 
     Libreria para crear hilos: Thread
@@ -8,9 +13,11 @@
 
 from threading import Thread
 
+
 def worker(command: list) -> None:
-    for i in range(command): # type: ignore        
+    for i in range(command):  # type: ignore
         print(f"Thread {i} - funcionando")
+
 
 new_hilo: Thread = Thread(target=worker, args=(10,))
 new_hilo.start()
