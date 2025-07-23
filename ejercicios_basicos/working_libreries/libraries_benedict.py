e8dbcb48ed37e5d90b32181186983d65c1daaa8b
JuanDuran85 <duran85juan@gmail.com>
2025-07-23 18:52:57 -0400

    Add 'keyattr' to spell check words in settings.json and update libraries_benedict.py documentation and functionality

--- ejercicios_basicos/working_libreries/libraries_benedict.py
+++ ejercicios_basicos/working_libreries/libraries_benedict.py
@@ -5,7 +5,20 @@
 
 """_summary_
 
-    Working with colorsys library
+    Working with benedict library
 
 """
 
+from benedict import benedict
+
+d = benedict(keyattr_dynamic=True)  # default False
+d.profile.firstname = "John"
+d.profile.lastname = "Doe"
+print(d)
+
+
+data_dict = benedict({'abc': 1}, format='json')
+print(data_dict['abc'])
+print(data_dict.get('abc'))
+data_dict.flatten()
+print(data_dict)
